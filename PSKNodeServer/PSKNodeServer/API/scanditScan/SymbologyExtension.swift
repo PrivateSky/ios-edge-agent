ce5f05f8207ac82a0fa7a2d08a276f7d1903421b
Zrust, Vladimir <vladimir.zrust@merck.com>
2021-06-24 13:50:14 +0200

    Add Symbology extension with updated description strings

---
+++ PSKNodeServer/PSKNodeServer/API/scanditScan/SymbologyExtension.swift
@@ -0,0 +1,25 @@
+//
+//  SymbologyExtension.swift
+//  PSKNodeServer
+//
+//  Created by Zrust, Vladimir on 24.06.2021.
+//
+
+import ScanditBarcodeCapture
+
+extension Symbology {
+    // Some descriptions do not comply with documentation - this extension is to keep consistency for used symbologies
+    // https://docs.scandit.com/stable/web/enums/barcode.symbology-1.html#micro_pdf417
+    var updatedDescription: String {
+        switch self {
+        case .gs1DatabarLimited:
+            return "databar-limited"
+        case .dataMatrix:
+            return "data-matrix"
+        case .microPDF417:
+            return "micropdf417"
+        default:
+            return self.description
+        }
+    }
+}
