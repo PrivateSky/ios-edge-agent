5144450ec2d62628451598224b32ea1e81453bac
Zrust, Vladimir <vladimir.zrust@merck.com>
2021-06-24 14:48:28 +0200

    Add ScnaditError case for scanCancelled

--- PSKNodeServer/PSKNodeServer/API/scanditScan/ScanditError.swift
+++ PSKNodeServer/PSKNodeServer/API/scanditScan/ScanditError.swift
@@ -9,11 +9,14 @@ import Foundation
 
 enum ScanditError: Error {
     case unableToRecognizeCompositeCode
+    case scanCancelled
     
     var description: String {
         switch self {
         case .unableToRecognizeCompositeCode:
             return "Reached limit for repeating scan of composite codes"
+        case .scanCancelled:
+            return "User cancelled the scan"
         }
     }
 }
